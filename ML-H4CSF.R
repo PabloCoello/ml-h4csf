b88e7dad6d392b9c6dfaa1ccd97aeab8324b9d0e
Pablo C <54448478+PabloCoello@users.noreply.github.com>
2019-09-24 23:16:52 +0200

    Update ML-H4CSF.R

--- ML-H4CSF.R
+++ ML-H4CSF.R
@@ -20,21 +20,9 @@ zIntercept = TRUE
 it = c("port", "time")
 
 
-data(usmanuf)
-usmanuf = usmanuf[usmanuf$naics!="311330",]
-usmanuf = usmanuf[usmanuf$naics!="311511",]
-usmanuf = usmanuf[usmanuf$naics!="311513",]
-usmanuf = usmanuf[usmanuf$naics!="311611",]
-usmanuf = usmanuf[usmanuf$naics!="311612",]
-usmanuf = usmanuf[usmanuf$naics!="311615",]
-usmanuf = usmanuf[usmanuf$naics!="311811",]
-usmanuf = usmanuf[usmanuf$naics!="312111",]
-usmanuf = usmanuf[usmanuf$naics!="313311",]
-usmanuf = usmanuf[usmanuf$naics!="314121",]
-usmanuf = usmanuf[usmanuf$naics!="315999",]
-usmanuf = usmanuf[usmanuf$naics!="321113",]
-usmanuf = usmanuf[usmanuf$naics!="322121",]
 
+
+data(usmanuf)
 na = (as.character(unique(usmanuf[which(is.na(usmanuf$Y)),"naics"])))
 for (n in na){
   usmanuf = usmanuf[usmanuf$naics!=n,]
@@ -59,7 +47,7 @@ data = usmanuf
 get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
   Y_ = data.frame(log(data[,yName]))
   X_ = data.frame(log(data[,xNames]))
-  Z_ = data.frame(log(data[,zNames]))
+  Z_ = data.frame(data[,zNames])
   
   nb = length(X_)
   ng = length(Z_)
@@ -80,13 +68,13 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
                    mget(ls(pattern="gamma")), 
                    mget(ls(pattern="sigmaesq")))
   
-  index = unique(data[,"naics"])
+  index = unique(data[,it[1]])
   time = unique(data[,"time"])
   dataY= cbind.data.frame(Y_, data[,it[1]], data$time)
   dataX= cbind.data.frame(X_, data[,it[1]], data$time)
   dataZ= cbind.data.frame(Z_, data[,it[1]], data$time)
   
-  log_likelihood = function(beta1, beta2, gamma1, gamma2, sigmaesq){
+  log_likelihood = function(beta1, beta2, beta3, beta4, beta5, gamma1, gamma2, gamma3, sigmaesq){
     t=length(time)
     get_p_matrix = function(t){
       p = -diag(t)
@@ -96,26 +84,28 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
       p = p+aux
       return(p)
     }
-    get_beta_array = function(coefficients, nb){
-      beta = array(dim = nb)
-      for (i in 1:nb){
-        beta[i] = coefficients[[i]]
-      }
-      return(beta)
-    }
-    get_gamma_coefficients = function(coefficients, nb, ng){
-      gamma = array(dim = ng)
-      count = 0
-      for (i in (nb+1):(length(coefficients)-1)){
-        count = count + 1
-        gamma[count] = coefficients[[i]]
-      }
-      return(gamma)
-    }
+#    get_beta_array = function(coefficients, nb){
+#      beta = array(dim = nb)
+#      for (i in 1:nb){
+#        beta[i] = coefficients[[i]]
+#      }
+#      return(beta)
+#    }
+#    get_gamma_coefficients = function(coefficients, nb, ng){
+#      gamma = array(dim = ng)
+#      count = 0
+#      for (i in (nb+1):(length(coefficients)-1)){
+#        count = count + 1
+#        gamma[count] = coefficients[[i]]
+#      }
+#      return(gamma)
+#    }
     
-    beta_array = get_beta_array(coefficients, nb)
-    gamma_array = get_gamma_coefficients(coefficients, nb, ng)
-    sigmaesq = coefficients[[length(coefficients)]]
+    #beta_array = get_beta_array(coefficients, nb)
+    beta_array = c(beta1,beta2,beta3,beta4,beta5)
+    #gamma_array = get_gamma_coefficients(coefficients, nb, ng)
+    gamma_array = c(gamma1,gamma2,gamma3)
+    #sigmaesq = coefficients[[length(coefficients)]]
     
     P = get_p_matrix(t)
     result = array(dim = length(index))
@@ -148,16 +138,21 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
     -sum(log(result))
   }
   
-  ml = mle2(log_likelihood, start = coefficients,
+  ml = mle2(log_likelihood, start = list(beta1=1,beta2=1,beta3=1,beta4=1,beta5=1,gamma1=0.001,gamma2=0.001,gamma3=0.001,gamma4=0.001,gamma5=0.001,gamma6=0.001,sigmaesq=1),
        method = "L-BFGS-B",
        trace = TRUE,
-       lower = c(beta1 =-Inf, beta2=-Inf, gamma1=-Inf, gamma1=-Inf, sigmaesq = 0.001),
-       upper = c(beta1 =Inf, beta2=Inf, gamma1=Inf, gamma1=Inf, sigmaesq = 10))
+       lower = c(beta1 =-Inf, beta2=-Inf,beta3=-Inf,beta4=-Inf,beta5=-Inf, gamma1=-Inf, gamma2=-Inf,gamma3=-Inf, sigmaesq = 0.001),
+       upper = c(beta1 =Inf, beta2=Inf,beta3=Inf,beta4=Inf,beta5=Inf, gamma1=Inf, gamma2=Inf,gamma3=Inf, sigmaesq = 10))
   return(ml)
 }
 
 
-
+res = get_ml_estimation(yName = "total_traffic",
+                        xNames = c("total_meters", "tugs", "total_cranes", "storages_total", "wages"),
+                        zNames = c("averagehs", "averagewind"),
+                        zIntercept = TRUE,
+                        it = c("port", "time"),
+                        data = data)
 
 
 
