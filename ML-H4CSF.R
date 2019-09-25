aeb80f0e2c2cb0798e5e99b749af2e202619bf95
Pablo C <pablo.coello@outlook.es>
2019-09-25 11:53:55 +0200

    Update ML-H4CSF.R

--- ML-H4CSF.R
+++ ML-H4CSF.R
@@ -14,34 +14,34 @@ data <- read_excel("Puertos_data.xlsx")
 data <- pdata.frame(data,  index = "port")
 
 yName = "total_traffic"
-xNames = c("total_meters", "tugs")
-zNames = c("averagehs", "systems_hs")
+xNames = c("total_meters", "tugs", "total_cranes", "storages_total", "wages")
+zNames = c("averagehs", "averagewind")
 zIntercept = TRUE
 it = c("port", "time")
 
 
 
-
-data(usmanuf)
-na = (as.character(unique(usmanuf[which(is.na(usmanuf$Y)),"naics"])))
-for (n in na){
-  usmanuf = usmanuf[usmanuf$naics!=n,]
-}
-
-
-usmanuf <- pdata.frame(usmanuf,  index = "naics")
-res = get_ml_estimation(yName = "Y",
-                  xNames = c("K", "L"),
-                  zNames = c("M"),
-                  zIntercept = TRUE,
-                  it = c("naics", "time"),
-                  data = usmanuf)
-yName = "Y"
-xNames = c("K", "L")
-zNames = c("M")
-zIntercept = TRUE
-it = c("naics", "time")
-data = usmanuf
+#
+#data(usmanuf)
+#na = (as.character(unique(usmanuf[which(is.na(usmanuf$Y)),"naics"])))
+#for (n in na){
+#  usmanuf = usmanuf[usmanuf$naics!=n,]
+#}
+#
+#
+#usmanuf <- pdata.frame(usmanuf,  index = "naics")
+#res = get_ml_estimation(yName = "Y",
+#                  xNames = c("K", "L"),
+#                  zNames = c("M"),
+#                  zIntercept = TRUE,
+#                  it = c("naics", "time"),
+#                  data = usmanuf)
+#yName = "Y"
+#xNames = c("K", "L")
+#zNames = c("M")
+#zIntercept = TRUE
+#it = c("naics", "time")
+#data = usmanuf
 
 
 get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
@@ -74,13 +74,22 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
   dataX= cbind.data.frame(X_, data[,it[1]], data$time)
   dataZ= cbind.data.frame(Z_, data[,it[1]], data$time)
   
-  log_likelihood = function(beta1, beta2, beta3, beta4, beta5, gamma1, gamma2, gamma3, sigmaesq){
+  log_likelihood = function(beta1, beta2, beta3, beta4, beta5, gamma1, gamma2, sigmaesq){
+    print(paste(beta1, beta2, beta3, gamma1, gamma2, sigmaesq))
     t=length(time)
     get_p_matrix = function(t){
       p = -diag(t)
       p = p[-nrow(p),]
       aux = diag(t)
       aux = aux[-1,]
+      p = p+aux
+      return(p)
+    }
+    get_mycoast_p_matrix = function(t){
+      p = -diag(t)
+      p = p[-c((nrow(p)-11):nrow(p)),]
+      aux = diag(t)
+      aux = aux[-c(1:12),]
       p = p+aux
       return(p)
     }
@@ -102,48 +111,55 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
 #    }
     
     #beta_array = get_beta_array(coefficients, nb)
-    beta_array = c(beta1,beta2,beta3,beta4,beta5)
+    beta_array = as.matrix(c(beta1,beta2,beta3,beta4,beta5))
     #gamma_array = get_gamma_coefficients(coefficients, nb, ng)
-    gamma_array = c(gamma1,gamma2,gamma3)
+    gamma_array = as.matrix(c(gamma1,gamma2))
     #sigmaesq = coefficients[[length(coefficients)]]
     
-    P = get_p_matrix(t)
+    P = get_mycoast_p_matrix(t)
+
     result = array(dim = length(index))
-    count = 0
-    for (i in index){
-      Y = as.matrix(dataY[which(dataY$`data[, it[1]]`==i),1])
-      X = as.matrix(dataX[which(dataX$`data[, it[1]]`==i),1:nb])
-      Z = as.matrix(dataZ[which(dataZ$`data[, it[1]]`==i),1:ng])
+
+    for (i in 1:length(index)){
+      Y = as.matrix(dataY[which(dataY$`data[, it[1]]`==index[i]),1])
+      X = as.matrix(dataX[which(dataX$`data[, it[1]]`==index[i]),1:nb])
+      Z = as.matrix(dataZ[which(dataZ$`data[, it[1]]`==index[i]),1:ng])
       y = P%*%Y
       x = P%*%X
       R = y - x%*%beta_array
       
-      var_array = array(dim=t)
+      var_array = as.matrix(array(dim=t))
       for (j in 1:t){
-        var_array[j]=exp(gamma_array%*%Z[j,])
+        var_array[j]=exp(Z[j,]%*%gamma_array)
       }
       
+      var_ui = as.matrix(diag(array(var_array)))
+      var_vi = as.matrix(exp(sigmaesq)*diag(t))
       
-      mu = replicate(t-1, 0)
-      sigma = as.matrix(P%*%(sigmaesq*diag(t) + diag(var_array))%*%t(P))
-      rownames(sigma) <- colnames(sigma)
-      gamma = -diag(var_array)%*%t(P)%*%solve(sigma)
+      mu = replicate(t-12, 0)
+      sigma = as.matrix(P%*%(var_vi + var_ui)%*%t(P))
+      gamma = -var_ui%*%t(P)%*%solve(sigma)
       nu = replicate(t, 0)
-      delta = diag(var_array)-diag(var_array)%*%t(P)%*%solve(sigma)%*%P%*%diag(var_array)
+      delta = var_ui-var_ui%*%t(P)%*%solve(sigma)%*%P%*%var_ui
       
-      count = count + 1
       #result[count] = dcsn(x=array(R), mu, sigma, gamma, nu, delta)
-      result[count] = loglcsn(x=array(R), mu, sigma, gamma, nu, delta)
+      result[i] = loglcsn(x=array(R), mu, sigma, gamma, nu, delta)
     }
     print(-sum(result))
     -sum(result)
   }
   
-  ml = mle2(log_likelihood, start = list(beta1=1,beta2=1,beta3=1,beta4=1,beta5=1,gamma1=0.001,gamma2=0.001,gamma3=0.001,sigmaesq=1),
+  ml = mle2(log_likelihood, start = list(beta1=1,beta2=1,beta3=1,beta4=1,beta5=1,
+                                         gamma1=0.001,gamma2=0.001,
+                                         sigmaesq=1),
        method = "L-BFGS-B",
        trace = TRUE,
-       lower = c(beta1 =-Inf, beta2=-Inf,beta3=-Inf,beta4=-Inf,beta5=-Inf, gamma1=-Inf, gamma2=-Inf,gamma3=-Inf, sigmaesq = 0.001),
-       upper = c(beta1 =Inf, beta2=Inf,beta3=Inf,beta4=Inf,beta5=Inf, gamma1=Inf, gamma2=Inf,gamma3=Inf, sigmaesq = 10))
+       lower = c(beta1 =-Inf, beta2=-Inf,beta3=-Inf,beta4=-Inf,beta5=-Inf,
+                 gamma1=-Inf, gamma2=-Inf, 
+                 sigmaesq = 0.001),
+       upper = c(beta1 =Inf, beta2=Inf,beta3=Inf,beta4=Inf,beta5=Inf,
+                 gamma1=Inf, gamma2=Inf, 
+                 sigmaesq = 10))
   return(ml)
 }
 
@@ -151,7 +167,7 @@ get_ml_estimation = function(yName, xNames, zNames, zIntercept, data, it){
 res = get_ml_estimation(yName = "total_traffic",
                         xNames = c("total_meters", "tugs", "total_cranes", "storages_total", "wages"),
                         zNames = c("averagehs", "averagewind"),
-                        zIntercept = TRUE,
+                        zIntercept = FALSE,
                         it = c("port", "time"),
                         data = data)
 
